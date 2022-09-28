d6da4e81cdd331fc0ed1ef507828e66b5ddc5d90
thomassonneveldt <t.s.j.sonneveldt@tilburguniversity.edu>
2022-09-28 20:21:05 +0200

    updated code for antwerp

--- src/data-preparation/antwerp.R
+++ src/data-preparation/antwerp.R
@@ -1,7 +1,3 @@
-
-remove(list=ls())
-cat("\f")
-
 dir <- "C:/Universiteit/Skills Data Prep. & Workflow Mgt/"
 setwd(dir)
 
@@ -43,30 +39,67 @@ antwerp_lis <- read_csv(file = paste0(dirdata, 'belgiumvlgantwerpdatalistingscsv
 antwerp_cal <- read_csv(file = paste0(dirdata, 'belgiumvlgantwerpdatacalendarcsvgz.csv'))
 
 # Turn price into a numeric value
+antwerp_cal$price <- as.numeric(gsub('\\$|,', '', antwerp_cal$price))
 antwerp_lis$price <- as.numeric(gsub('\\$|,', '', antwerp_lis$price))
-
 # Select 3 important columns
-antwerp_cal <- antwerp_cal %>% select(listing_id, date, available)
+antwerp_cal <- antwerp_cal %>% select(listing_id, date, price)
 
 # Pivot the data frame wide
 antwerp_cal_wide <- antwerp_cal %>% 
   
   pivot_wider(names_from = date,
-              values_from = available,
+              values_from = price,
   )
 
-# Select only the columns with the listing id and dates from 2022-08-15 to 2022-09-15  
-antwerp_cal_wide_clean <- select(antwerp_cal_wide, listing_id, "2022-08-15":"2022-09-15")
+# Select only the columns with the listing id and dates from 2022-08-15 to 2022-09-15 and 2023-03-15 to 2023-04-15 
+antwerp_cal_wide_clean <- select(antwerp_cal_wide, listing_id, "2022-08-15":"2022-09-15", "2023-03-15":"2023-04-15")
 
-# Count number of times a listing is available and not available
+# Take the average price per listing from August/September and March/April
 antwerp_cal_wide_clean <- antwerp_cal_wide_clean %>% 
-  mutate(num_true = rowSums(antwerp_cal_wide_clean == TRUE)) %>% 
-  mutate(num_false = rowSums(antwerp_cal_wide_clean == FALSE))
-                                                                
-#have to rename listing_id to id so can be used to merge 
+  mutate(average_price_aug = rowMeans(antwerp_cal_wide_clean[,2:33])) %>% 
+  mutate(average_price_mar = rowMeans(antwerp_cal_wide_clean[,34:65]))
+
+antwerp_cal_wide_clean$average_price_aug  <- round(antwerp_cal_wide_clean$average_price_aug, digits = 2)
+antwerp_cal_wide_clean$average_price_mar  <- round(antwerp_cal_wide_clean$average_price_mar, digits = 2)
+# Rename listing_id to id so it can be used to merge 
 antwerp_cal_wide_clean <- antwerp_cal_wide_clean %>%  rename(id = listing_id)
-#merging the dataframes 
+# Select only useful columns from listings dataset and calendar data set
+antwerp_lis <- antwerp_lis %>% select(id, room_type, price, minimum_nights, maximum_nights, host_is_superhost)
+antwerp_cal_wide_clean <- antwerp_cal_wide_clean %>% select(id, average_price_aug, average_price_mar)
+# Merge the dataframes 
 total_antwerp <- merge(antwerp_cal_wide_clean, antwerp_lis, by="id")
+
+# Read dataset again to get all columns back
+antwerp_cal <- read_csv(file = paste0(dirdata, 'belgiumvlgantwerpdatacalendarcsvgz.csv'))
+# Select 3 important columns
+antwerp_cal <- antwerp_cal %>% select(listing_id, date, minimum_nights)
+
+# Pivot the data frame wide
+antwerp_cal_wide <- antwerp_cal %>% 
+  
+  pivot_wider(names_from = date,
+              values_from = minimum_nights,
+  )
+
+# Select only the columns with the listing id and dates from 2022-08-15 to 2022-09-15 and 2023-03-15 to 2023-04-15 
+antwerp_cal_wide_clean <- select(antwerp_cal_wide, listing_id, "2022-08-15":"2022-09-15", "2023-03-15":"2023-04-15")
+
+# Take the average price per listing from August/September and March/April
+antwerp_cal_wide_clean <- antwerp_cal_wide_clean %>% 
+  mutate(average_nights_aug = rowMeans(antwerp_cal_wide_clean[,2:33])) %>% 
+  mutate(average_nights_mar = rowMeans(antwerp_cal_wide_clean[,34:65]))
+
+antwerp_cal_wide_clean$average_nights_aug  <- round(antwerp_cal_wide_clean$average_nights_aug, digits = 2)
+antwerp_cal_wide_clean$average_nights_mar  <- round(antwerp_cal_wide_clean$average_nights_mar, digits = 2)
+
+# Rename listing_id to id so it can be used to merge 
+antwerp_cal_wide_clean <- antwerp_cal_wide_clean %>%  rename(id = listing_id)
+
+# Select only useful columns from calendar data set
+antwerp_cal_wide_clean <- antwerp_cal_wide_clean %>% select(id, average_nights_aug, average_nights_mar)
+
+# Merge the dataframes 
+total_antwerp <- merge(antwerp_cal_wide_clean, total_antwerp, by="id")
 
 # Save dataset
 write_csv(total_antwerp, file = paste0(dirdata_output, "total_antwerp.csv"), append = FALSE, col_names = TRUE)
