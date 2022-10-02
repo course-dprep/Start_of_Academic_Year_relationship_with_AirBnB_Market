912d52f5b90fbb09ba73e6cf0b570b3c58f31d4b
thomassonneveldt <t.s.j.sonneveldt@tilburguniversity.edu>
2022-10-02 21:22:44 +0200

    cleaned up some text

--- src/data-preparation/all_cities.R
+++ src/data-preparation/all_cities.R
@@ -9,7 +9,7 @@ lis_datasets <- lapply(lis_urls, read_csv)
 cal_datasets <- lapply(cal_urls, read_csv)
 
 
-
+# Create function that calculates average price in August/September and March/April and then only extracts those columns with the id column  
 clean_cal_price <- function (dataset) {
 
   # Turn price into a numeric value
@@ -17,7 +17,7 @@ clean_cal_price <- function (dataset) {
     # Select 3 important columns
   dataset_clean <- dataset %>% select(listing_id, date, price)
   
-  # Pivot the data frame wide
+  # Pivot the data set wide
   dataset_wide <- dataset_clean %>% 
     
     pivot_wider(names_from = date,
@@ -43,14 +43,15 @@ clean_cal_price <- function (dataset) {
   dataset_wide_clean <- dataset_wide_clean %>% select(id, average_price_aug, average_price_mar)
 }  
 
-# Clean up all calendar datasets and only extract the average price in august/september and march/april and put them into a new list
+# Clean up all calendar data sets and only extract the average price in August/September and March/April and put them into a new list
 clean_cal_price_datasets <- lapply(cal_datasets, clean_cal_price)
 
+# Create function that calculates average minimum nights in August/September and March/April and then only extracts those columns with the id column  
 clean_cal_nights <- function(dataset){
   # Select 3 important columns
   dataset_clean <- dataset %>% select(listing_id, date, minimum_nights)
   
-  # Pivot the data frame wide
+  # Pivot the data set wide
   dataset_wide <- dataset_clean %>% 
     
     pivot_wider(names_from = date,
@@ -76,27 +77,28 @@ clean_cal_nights <- function(dataset){
   
 }
 
-# Clean up all calendar data sets and only extract the average minimum nights in august/september and march/april and put them into a new list
+# Clean up all calendar data sets and only extract the average minimum nights in August/September and March/April and put them into a new list
 clean_cal_nights_datasets <- lapply(cal_datasets, clean_cal_nights)
 
+# Create function that only selects relevant columns from the listings data set
 clean_lis <- function(dataset) {
   # Turn price into a numeric value
   dataset$price <- as.numeric(gsub('\\$|,', '', dataset$price))
-  # Select only useful columns from listings dataset
+  # Select only useful columns from listings data set
   dataset <- dataset %>% select(id, room_type, price, minimum_nights, maximum_nights, host_is_superhost)
 }
 
 # Clean all listings data sets and put them into a new list
 clean_lis_datasets <- lapply(lis_datasets, clean_lis)
 
-# Merge the data frames 
+# Merge the data sets 
 total_antwerp <- merge(clean_cal_price_datasets[[1]], clean_cal_nights_datasets[[1]]) %>%  merge(clean_lis_datasets[[1]])
 total_amsterdam <-  merge(clean_cal_price_datasets[[2]], clean_cal_nights_datasets[[2]]) %>%  merge(clean_lis_datasets[[2]])
 total_rotterdam <-  merge(clean_cal_price_datasets[[3]], clean_cal_nights_datasets[[3]]) %>%  merge(clean_lis_datasets[[3]])
 total_brussels <-  merge(clean_cal_price_datasets[[4]], clean_cal_nights_datasets[[4]]) %>%  merge(clean_lis_datasets[[4]])
 total_berlin <-  merge(clean_cal_price_datasets[[5]], clean_cal_nights_datasets[[5]]) %>%  merge(clean_lis_datasets[[5]])
 
-# Write all data frames as .csv file
+# Write all data sets as .csv file
 write_csv(total_antwerp, "total_antwerp.csv")
 write_csv(total_amsterdam, "total_amsterdam.csv")
 write_csv(total_rotterdam, "total_rotterdam.csv")
