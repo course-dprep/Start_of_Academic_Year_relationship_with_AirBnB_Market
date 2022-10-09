ec24402bee723262c62c7a2e800ebc6aee1143a3
thomassonneveldt <t.s.j.sonneveldt@tilburguniversity.edu>
2022-10-09 12:45:11 +0200

    download datasets seperated from main code

--- src/data-preparation/Data_exploration_and_lm_kversion.R
+++ src/data-preparation/Data_exploration_and_lm_kversion.R
@@ -2,12 +2,6 @@ library(dplyr)
 library(tidyr)
 library(tidyverse)
 
-lis_urls <- c('http://data.insideairbnb.com/belgium/vlg/antwerp/2022-06-22/data/listings.csv.gz', 'http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-06-05/data/listings.csv.gz', 'http://data.insideairbnb.com/the-netherlands/south-holland/rotterdam/2022-06-15/data/listings.csv.gz', 'http://data.insideairbnb.com/belgium/bru/brussels/2022-06-15/data/listings.csv.gz', 'http://data.insideairbnb.com/germany/be/berlin/2022-06-13/data/listings.csv.gz')
-cal_urls <- c('http://data.insideairbnb.com/belgium/vlg/antwerp/2022-06-22/data/calendar.csv.gz', 'http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-06-05/data/calendar.csv.gz', 'http://data.insideairbnb.com/the-netherlands/south-holland/rotterdam/2022-06-15/data/calendar.csv.gz', 'http://data.insideairbnb.com/belgium/bru/brussels/2022-06-15/data/calendar.csv.gz', 'http://data.insideairbnb.com/germany/be/berlin/2022-06-13/data/calendar.csv.gz')
-
-lis_datasets <- lapply(lis_urls, read_csv)
-cal_datasets <- lapply(cal_urls, read_csv)
-
 
 # Create function that calculates average price in August/September and then only extracts those columns with the id column  
 clean_cal_price_aug <- function (dataset) {
