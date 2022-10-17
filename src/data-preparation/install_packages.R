589d5dc164a932245f8ed69553337152ad34043d
thomassonneveldt <t.s.j.sonneveldt@tilburguniversity.edu>
2022-10-17 02:25:17 +0200

    added rmarkdown to makefile

--- src/data-preparation/install_packages.R
+++ src/data-preparation/install_packages.R
@@ -4,6 +4,7 @@ install.packages("dplyr", repos = "http://cran.us.r-project.org")
 install.packages("tidyr", repos = "http://cran.us.r-project.org")
 install.packages("tidyverse", repos = "http://cran.us.r-project.org")
 install.packages("Hmisc", repos = "http://cran.us.r-project.org")
+tinytex::install_tinytex()
 
 library(tidyverse)
 
