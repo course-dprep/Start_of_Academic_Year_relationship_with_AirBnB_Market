03bab8a9d7e9c04a61bf7e8a160434f320dd8686
thomassonneveldt <t.s.j.sonneveldt@tilburguniversity.edu>
2022-10-17 22:10:42 +0200

    removed folders and removed pdf from clean up file

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -16,8 +16,8 @@ cal_urls <- c('https://github.com/thomassonneveldt/datasets_dprep/raw/main/datas
 lis_datasets <- lapply(lis_urls, read_csv)
 cal_datasets <- lapply(cal_urls, read_csv)
 
-download.file("https://user-images.githubusercontent.com/112591530/196250721-3a241662-ff75-49d7-80b2-5124e6455e42.jpg", destfile = "../../gen/paper/temp/model.png",mode = "wb") 
-download.file("https://user-images.githubusercontent.com/112591530/196049694-3585dc9d-d527-43d3-90ce-d4c8500ae052.png", destfile = "../../gen/paper/temp/airbnb.png",mode = "wb")
+download.file("https://user-images.githubusercontent.com/112591530/196250721-3a241662-ff75-49d7-80b2-5124e6455e42.jpg", destfile = "../../gen/paper/model.png",mode = "wb") 
+download.file("https://user-images.githubusercontent.com/112591530/196049694-3585dc9d-d527-43d3-90ce-d4c8500ae052.png", destfile = "../../gen/paper/airbnb.png",mode = "wb")
 
 save(cal_datasets,file="../../gen/data-preparation/temp/cal_datasets.RData")
 save(lis_datasets,file="../../gen/data-preparation/temp/lis_datasets.RData")
